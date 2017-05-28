213b8f27f8aae5d53892263f4aee053c31fbf199
Rafael Vicio <rafaelvicio@icloud.com>
2017-05-27 22:57:57 -0300

    Scroll infinito

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,31 +9,56 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-  public applicants: Array<string>;
-  private url: string = "https://jarbas.serenatadeamor.org/api/applicant/";
-
-  itemSelected (feed):void {
-    alert(feed.data.url);
-  }
+  public feeds: Array<any>;
+  private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
 
   constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
 
-     this.fetchContent();
+    this.fetchContent();
 
   }
 
+  // Loagin
   fetchContent ():void {
     let loading = this.loadingCtrl.create({
-      content: 'Carregando...'
+      content: 'Fetching content...'
     });
 
     loading.present();
 
     this.http.get(this.url).map(res => res.json())
       .subscribe(data => {
-        this.applicants = data.results;
+        this.feeds = data.data.children;
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        })
+
         loading.dismiss();
       });
+  }
 
+  // Scroll infinito
+  doInfinite(infiniteScroll) {
+
+    let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1].data.name : "";
+
+      this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
+        .subscribe(data => {
+
+          this.feeds = this.feeds.concat(data.data.children);
+
+          this.feeds.forEach((e, i, a) => {
+            if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+            }
+          })
+
+          infiniteScroll.complete();
+        });
 }
+
 }
